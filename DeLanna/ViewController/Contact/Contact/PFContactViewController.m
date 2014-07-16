5e0ddd791a726a78dd2388ebe9b5b12c50257e23
Pariwat <wat_promjai@hotmail.com>
2014-07-16 14:47:52 +0700

    up
    up

---
+++ DeLanna/ViewController/Contact/Contact/PFContactViewController.m
@@ -0,0 +1,204 @@
+//
+//  PFContactViewController.m
+//  DeLanna
+//
+//  Created by Pariwat on 7/15/14.
+//  Copyright (c) 2014 Platwo fusion. All rights reserved.
+//
+
+#import "PFContactViewController.h"
+
+@interface PFContactViewController ()
+
+@end
+
+@implementation PFContactViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Navbar setup
+    [[self.navController navigationBar] setBarTintColor:[UIColor colorWithRed:212.0f/255.0f green:185.0f/255.0f blue:0.0f/255.0f alpha:1.0f]];
+    
+    [[self.navController navigationBar] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                                 [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0], NSForegroundColorAttributeName, nil]];
+    
+    [[self.navController navigationBar] setTranslucent:YES];
+    [self.view addSubview:self.navController.view];
+    
+    self.tableView.tableHeaderView = self.headerView;
+    self.tableView.tableFooterView = self.footerView;
+    
+    CALayer *buttonView = [self.buttonView layer];
+    [buttonView setMasksToBounds:YES];
+    [buttonView setCornerRadius:7.0f];
+    
+    CALayer *commentView = [self.commentView layer];
+    [commentView setMasksToBounds:YES];
+    [commentView setCornerRadius:7.0f];
+    
+    CALayer *reserveButton = [self.reserveButton layer];
+    [reserveButton setMasksToBounds:YES];
+    [reserveButton setCornerRadius:7.0f];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+-(NSUInteger)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (IBAction)mapTapped:(id)sender{
+    
+    [self.delegate HideTabbar];
+    
+    PFMapViewController *mapView = [[PFMapViewController alloc] init];
+    if(IS_WIDESCREEN) {
+        mapView = [[PFMapViewController alloc] initWithNibName:@"PFMapViewController_Wide" bundle:nil];
+    } else {
+        mapView = [[PFMapViewController alloc] initWithNibName:@"PFMapViewController" bundle:nil];
+    }
+    mapView.delegate = self;
+    [self.navController pushViewController:mapView animated:YES];
+}
+
+- (IBAction)phoneTapped:(id)sender{
+    //NSString *phone = [[NSString alloc] initWithFormat:@"telprompt://%@",[self.obj objectForKey:@"phone"]];
+    NSString *phone = [[NSString alloc] initWithFormat:@"telprompt://%@",@"0938405460"];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phone]];
+}
+
+- (IBAction)websiteTapped:(id)sender{
+    
+    [self.delegate HideTabbar];
+    
+    PFWebViewController *webView = [[PFWebViewController alloc] init];
+    if(IS_WIDESCREEN) {
+        webView = [[PFWebViewController alloc] initWithNibName:@"PFWebViewController_Wide" bundle:nil];
+    } else {
+        webView = [[PFWebViewController alloc] initWithNibName:@"PFWebViewController" bundle:nil];
+    }
+    webView.delegate = self;
+    [self.navController pushViewController:webView animated:YES];
+}
+
+- (IBAction)emailTapped:(id)sender{
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:@"Select Menu"
+                                  delegate:self
+                                  cancelButtonTitle:@"cancel"
+                                  destructiveButtonTitle:nil
+                                  otherButtonTitles:@"Send Email", nil];
+    [actionSheet showInView:[[[[UIApplication sharedApplication] keyWindow] subviews] lastObject]];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    //Get the name of the current pressed button
+    NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+    
+    if  ([buttonTitle isEqualToString:@"Send Email"]) {
+        [self.delegate HideTabbar];
+        NSLog(@"Send Email");
+        // Email Subject
+        NSString *emailTitle = @"De Lanna Hotel";
+        // Email Content
+        NSString *messageBody = @"De Lanna Hotel!";
+        // To address
+        //NSArray *toRecipents = [NSArray arrayWithObject:[self.obj objectForKey:@"email"]];
+        NSArray *toRecipents = [NSArray arrayWithObject:@""];
+        
+        [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:212.0f/255.0f green:185.0f/255.0f blue:0.0f/255.0f alpha:1.0f]];
+        
+        [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                               [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0], NSForegroundColorAttributeName, nil]];
+        
+        MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+        mc.mailComposeDelegate = self;
+        
+        [mc.navigationBar setTintColor:[UIColor whiteColor]];
+        [mc setSubject:emailTitle];
+        [mc setMessageBody:messageBody isHTML:NO];
+        [mc setToRecipients:toRecipents];
+        
+        // Present mail view controller on screen
+        [self presentViewController:mc animated:YES completion:NULL];
+        
+    }
+    if ([buttonTitle isEqualToString:@"Cancel"]) {
+        NSLog(@"Cancel");
+    }
+}
+
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            //[self reloadView];
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    // Close the Mail Interface
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    [self.delegate ShowTabbar];
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField  {
+    [self.commentText resignFirstResponder];
+    return YES;
+}
+
+- (IBAction)reserveTapped:(id)sender{
+    
+    [self.delegate HideTabbar];
+    
+    PFWebViewController *webView = [[PFWebViewController alloc] init];
+    if(IS_WIDESCREEN) {
+        webView = [[PFWebViewController alloc] initWithNibName:@"PFWebViewController_Wide" bundle:nil];
+    } else {
+        webView = [[PFWebViewController alloc] initWithNibName:@"PFWebViewController" bundle:nil];
+    }
+    webView.delegate = self;
+    [self.navController pushViewController:webView animated:YES];
+}
+
+- (IBAction)powerbyTapped:(id)sender{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://pla2fusion.com/"]];
+}
+
+- (void) PFMapViewControllerBack {
+    [self.delegate ShowTabbar];
+}
+
+- (void) PFWebViewControllerBack {
+    [self.delegate ShowTabbar];
+}
+
+@end
